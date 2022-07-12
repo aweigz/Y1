cbe611865a2ffbf3f896df6f56f794fa487083b1
aweigz <aweigz@gmail.com>
2022-07-13 00:38:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,4 +23,4 @@
 #https://github.com/xiaorouji/openwrt-passwall2.git
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
